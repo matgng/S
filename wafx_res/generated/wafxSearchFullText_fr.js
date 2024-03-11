e2de6c325e6cd816d7e746c8c76b00437e26ca44
matgng <160502903+matgng@users.noreply.github.com>
2024-03-11 15:25:25 -0300

    Add files via upload

--- wafx_res/generated/wafxSearchFullText_fr.js
+++ wafx_res/generated/wafxSearchFullText_fr.js
@@ -1,9 +1,10 @@
  { 
 "map_targets":[
- {  "title":"Page sans titre" , "display_url":"index.html" , "url":"index.html" } 
+ {  "title":"Page sans titre" , "display_url":"20d2539d_05d2_42ec_891e_a54c39a4bc20.html" , "url":"20d2539d_05d2_42ec_891e_a54c39a4bc20.html" } 
+ , {  "title":"Page sans titre" , "display_url":"index.html" , "url":"index.html" } 
 ],
 "map_words":[""]
 ,
-"index_words":[ [0 ] 
+"index_words":[ [1,0 ] 
 ]
  } 
